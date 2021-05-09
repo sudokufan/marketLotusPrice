f4279a96c73e249c758fee13d3237f1c615333a4
sudokufan <alexrichardgeorge@protonmail.com>
2021-05-09 17:55:31 +0100

    starter centreing, changed API img call

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function fetchData() {
+function fetchLotusPrice() {
     fetch('https://api.scryfall.com/cards/named?exact=black+lotus&set=2ed')
         .then(function(data){
             data = data.json()
             return data
         }) .then(function(data) {
-            document.querySelector('.lotusCardScan').src = data.image_uris.normal
+            document.querySelector('.lotusCardScan').src = data.image_uris.border_crop
             document.querySelector('.lotusPrice').textContent = data.prices.usd
         })
 }
 
-fetchData()
+fetchLotusPrice()
